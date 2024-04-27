6532688b5140199862b488453c940841370b3bb6
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-04-27 21:10:26 +0530

    adding ai

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 import { MONGO_URI } from "@/env_variable";
-=======
->>>>>>> 2cf4e7788e8de4cf838a52b01a12de3d4726392b
 import mongoose from "mongoose";
 
 type ConnectionObject = {
@@ -17,14 +14,10 @@ async function dbconnect(): Promise<void> {
   }
 
   try {
-<<<<<<< HEAD
     const db = await mongoose.connect(
      MONGO_URI ||
         ""
     );
-=======
-    const db = await mongoose.connect(process.env.MONGO_URI || "");
->>>>>>> 2cf4e7788e8de4cf838a52b01a12de3d4726392b
 
     connection.isConnected = db.connections[0].readyState;
     console.log("Database connected");
@@ -35,9 +28,5 @@ async function dbconnect(): Promise<void> {
   }
 }
 
-<<<<<<< HEAD
-export default dbconnect;
-=======
 
 export default dbconnect 
->>>>>>> 2cf4e7788e8de4cf838a52b01a12de3d4726392b
