63bd60d67966aed05c8f52f535c336e1dcabf979
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-04-28 13:04:29 +0530

    creating delete message route

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 export const usernameValidation = z
   .string()
-  .min(5, "Username must be atleast 5 character")
-  .max(10, "Username must be no more than 10 character")
+  .min(2, "Username must be atleast 2 character")
+  .max(20, "Username must be no more than 20 character")
   .regex(/^[a-zA-Z0-9_]+$/, "Username must not contain special character");
 
 export const SignUpSchema = z.object({
