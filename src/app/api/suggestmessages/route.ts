59274ce3c53a292fa5668cd6180cead9784d02fa
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-05-02 21:00:20 +0530

    removing eslinting

--- src/app/api/suggestmessages/route.ts
+++ src/app/api/suggestmessages/route.ts
@@ -2,6 +2,11 @@ import OpenAI from "openai";
 import { OpenAIStream, StreamingTextResponse } from "ai";
 import { NextResponse } from "next/server";
 
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
