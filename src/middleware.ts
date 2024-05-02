7fe8c6c5bb390fb45db9de49fdc483caa5c3dc42
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-05-02 21:09:57 +0530

    removing imports

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,6 @@
 
 import { NextResponse, NextRequest } from "next/server";
-// export { default } from "next-auth/middleware";
 import { getToken } from "next-auth/jwt";
-import { NEXT_AUTH_SECRET } from "./env_variable";
 
 export async function middleware(request: NextRequest) {
   
