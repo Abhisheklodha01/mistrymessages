b51760ccc1fde2fd377ae10d574be9998b193c91
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-05-08 21:31:01 +0530

    solving errors

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -68,6 +68,7 @@ const LoginPage = () => {
   };
 
   return (
+    <>
     <div className="flex justify-center items-center min-h-screen bg-gray-800">
       <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
         <div className="text-center">
@@ -115,6 +116,7 @@ const LoginPage = () => {
         </div>
       </div>
     </div>
+    </>
   );
 };
 
